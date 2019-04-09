0ab930b9c49805e961262f845c238520a464f940
atregubenko <anna@egeniq.com>
2019-04-09 16:09:59 +0300

    added ListView without Java Colletions

--- app/src/main/java/com/example/studyjams2018/MainActivity.java
+++ app/src/main/java/com/example/studyjams2018/MainActivity.java
@@ -1,22 +1,28 @@
 package com.example.studyjams2018;
 
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.studyjams2018.adapters.ProductListAdapter;
 import com.example.studyjams2018.entity.Product;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-    private TextView totalAmount, price, amount, name;
-    private ImageView image;
-    private Button btnOrder;
-    private int _amount;
-    private Product _product;
+    private ListView list;
+    private Product[] products = new Product[5];
+    private TextView totalAmount;
+
+    private void initializeData() {
+        products[0] = new Product("Cappuccino", 23, R.drawable.ic_capuccino);
+        products[1] = new Product("Americano", 25, R.drawable.ic_americano);
+        products[2] = new Product("Espresso", 35, R.drawable.ic_espresso);
+        products[3] = new Product("Flat White", 30, R.drawable.ic_flat_white);
+        products[4] = new Product("Mocha", 31, R.drawable.ic_mocha);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,36 +30,38 @@ public class MainActivity extends AppCompatActivity{
 
         setContentView(R.layout.activity_main);
 
-        name = findViewById(R.id.product_name);
-        image = findViewById(R.id.product_image);
-        price = findViewById(R.id.product_price);
-        amount = findViewById(R.id.amount);
-        btnOrder = findViewById(R.id.btn_order);
-
         totalAmount = findViewById(R.id.total_amount_value);
+        list = findViewById(R.id.products_list);
 
-        _product = new Product("Cappuccino", 23, R.drawable.ic_capuccino);
-        image.setImageDrawable(ContextCompat.getDrawable(this, _product.getPhotoId()));
-        name.setText(_product.getName());
-        price.setText(String.valueOf(_product.getPrice()));
+        initializeData();
+
+        final ProductListAdapter adapter = new ProductListAdapter(products);
+        adapter.setListener(new ProductListAdapter.ProductAmountChangesListener() {
+            @Override
+            public void onChanged() {
+                updateCartView();
+            }
+        });
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Product product = adapter.getItem(position);
+                product.setAmount(product.getAmount() + 1);
+                updateCartView();
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
     private void updateCartView() {
         int tAmount = 0;
-        btnOrder.setVisibility(_amount == 0 ? View.VISIBLE : View.INVISIBLE);
-        amount.setText(String.valueOf(_amount));
-        tAmount += _amount * _product.getPrice();
+        for (Product product : products) {
+            Integer amount = product.getAmount();
+            tAmount += amount * product.getPrice();
+        }
         totalAmount.setText(String.valueOf(tAmount));
-    }
-
-    public void increaseAmount(View view) {
-        _amount++;
-        updateCartView();
-    }
-
-    public void decreaseAmount(View view) {
-        _amount--;
-        updateCartView();
     }
 }
 
